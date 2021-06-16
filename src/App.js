edf47ecc1d1be8f1688abed654579cd4da6f8f54
Riko Chair Nugroho <riko.chair@gmail.com>
2021-06-16 16:53:28 +0700

    add counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container } from 'react-bootstrap';
 
-function App() {
+import {useState} from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <div style={{display:'flex', justifyContent:'center', alignItems: 'center', flexDirection:'column', height:'100vh'}}>
+        <h2>{count}</h2>
+        Counter Angka Setiap Button di-click 1
+        <Button variant="primary" onClick={()=> setCount(count + 1)}>Button</Button>
+      </div>
+    </Container>
+  )
 }
 
 export default App;
